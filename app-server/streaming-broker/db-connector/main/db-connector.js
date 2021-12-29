08144a32a7a45012381b31de7641611887d0fd85
thiengo <long.hothiengo@hcmut.edu.vn>
2021-12-30 02:25:49 +0700

    update version 2 of streaming broker, remove multicaster

--- app-server/streaming-broker/db-connector/main/db-connector.js
+++ app-server/streaming-broker/db-connector/main/db-connector.js
@@ -16,15 +16,13 @@ const streaming_broker_port = common['emqx']['SERVER_PORT'];
 const dev_topic_levels = {
     'DEVICES': common['emqx']['TOPIC_LEVEL_DEVICES'],
     'UP': common['emqx']['TOPIC_LEVEL_UP'],
-    'METADATA': common['emqx']['TOPIC_LEVEL_METADATA'],
-    'PAYLOAD': common['emqx']['TOPIC_LEVEL_PAYLOAD']
+    'MIXED': common['emqx']['TOPIC_LEVEL_MIXED']
 };
 
 //get topics of all devices
-const dev_topics = {
-    'metadata': `${dev_topic_levels['DEVICES']}/+/${dev_topic_levels['UP']}/${dev_topic_levels['METADATA']}`,
-    'payload': `${dev_topic_levels['DEVICES']}/+/${dev_topic_levels['UP']}/${dev_topic_levels['PAYLOAD']}`
-};
+const sub_topics = [
+    `${dev_topic_levels['DEVICES']}/+/${dev_topic_levels['UP']}/${dev_topic_levels['MIXED']}`
+];
 
 /* ==============CONNECT TO STREAMING BROKER============== */
 const streaming_broker_mqttclient = mqtt.connect(
@@ -40,15 +38,70 @@ streaming_broker_mqttclient.on('message', streaming_broker_message_handler);
 function streaming_broker_connect_handler()
 {
     console.log(`streaming broker connected? ${streaming_broker_mqttclient.connected}`);
-    streaming_broker_mqttclient.subscribe(Object.values(dev_topics));
+    streaming_broker_mqttclient.subscribe(sub_topics);
     
 }
 
 //STORE TO DB
-function streaming_broker_message_handler(topic, message, packet)
+async function streaming_broker_message_handler(topic, message, packet)
 {
-    console.log(topic);
-    console.log(message.toString());
+    const parsed_message = JSON.parse(message);
+    try {
+        //insert enddev payload
+        await db_pool.query(
+            "INSERT INTO public.\"ENDDEV_PAYLOAD\" (recv_timestamp, payload_data, enddev_id)\
+            VALUES ($1, $2, (SELECT _id FROM public.\"ENDDEV\" WHERE dev_id = $3) )",
+            [
+                parsed_message['payload']['recv_timestamp'],
+                parsed_message['payload']['payload_data'],
+                parsed_message['metadata']['dev_identifiers']['dev_id']
+            ]
+        );
+    }
+    catch (err) {
+        //console.log(err.stack);
+        //if insert payload failed -> not exist enddev, then insert enddev
+        let res = await db_pool.query(
+            "INSERT INTO public.\"ENDDEV\" (display_name, dev_id, dev_addr, join_eui, dev_eui, dev_type, dev_brand, dev_model, dev_band)\
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING _id",
+            [
+                parsed_message['metadata']['dev_identifiers']['dev_id'],
+                parsed_message['metadata']['dev_identifiers']['dev_id'],
+                parsed_message['metadata']['dev_identifiers']['dev_addr'],
+                parsed_message['metadata']['dev_identifiers']['join_eui'],
+                parsed_message['metadata']['dev_identifiers']['dev_eui'],
+                parsed_message['metadata']['dev_version']['dev_type'],
+                parsed_message['metadata']['dev_version']['dev_brand'],
+                parsed_message['metadata']['dev_version']['dev_model'],
+                parsed_message['metadata']['dev_version']['dev_band']
+            ]
+        );
+        //get enddev_id for later used
+        const enddev_id = res.rows[0]['_id'];
+
+        //insert sensor_key
+        Object.keys(parsed_message['payload']['payload_data'])
+        .forEach(async (sensor_key) => {
+            await db_pool.query(
+                "INSERT INTO public.\"SENSOR\" (sensor_key, enddev_id)\
+                VALUES ($1, $2)",
+                [sensor_key, enddev_id]
+            );
+        });
+
+        //mapp appropriate unit to sensor
+
+        //insert payload again
+        await db_pool.query(
+            "INSERT INTO public.\"ENDDEV_PAYLOAD\" (recv_timestamp, payload_data, enddev_id)\
+            VALUES ($1, $2, (SELECT _id FROM public.\"ENDDEV\" WHERE dev_id = $3) )",
+            [
+                parsed_message['payload']['recv_timestamp'],
+                parsed_message['payload']['payload_data'],
+                parsed_message['metadata']['dev_identifiers']['dev_id']
+            ]
+        );
+    }
 }
 
 // handle error
@@ -57,3 +110,16 @@ function streaming_broker_error_handler(error)
     console.log("Can't connect to streaming broker" + error);
     process.exit(1);
 }
+
+/* ==============CONNECT TO PG============== */
+const {Pool} = require("pg");
+
+
+const db_pool = new Pool({
+    user: common["pg"]["POSTGRES_USER"],
+    password: common["pg"]["POSTGRES_PASSWORD"],
+    host: common["pg"]["SERVER_ADDR"],
+    port: common["pg"]["SERVER_PORT"],
+    database: common["pg"]["DATABASE_NAME"]
+})
+
