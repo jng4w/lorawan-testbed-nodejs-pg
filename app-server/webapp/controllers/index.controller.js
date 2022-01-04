9ab2b5b882cb64f1aed142a10184f72a155c5b01
Jing <jingnguyen10@gmail.com>
2022-01-04 23:16:44 +0700

    login processing update

--- app-server/webapp/controllers/index.controller.js
+++ app-server/webapp/controllers/index.controller.js
@@ -1,11 +1,11 @@
 const Index = require('../models/index.model');
 
 exports.indexProcessing = async (req, res, next) => {
-    // if(client_req.session.login){
+    res.redirect('/login'); 
+    // if(req.session.login){
     //     res.redirect('/dashboard');
     // }
     // else res.redirect('/login');    
-    res.redirect('/dashboard');
 }
 
 
--- app-server/webapp/controllers/login.controller.js
+++ app-server/webapp/controllers/login.controller.js
@@ -2,10 +2,26 @@ const Index = require('../models/index.model');
 const {validationResult } = require('express-validator');
 
 exports.loginProcessing = async (req, res, next) => {
-    console.log(req.body.uname, req.body.psw);
+    // console.log(req.body.uname, req.body.psw);
     // console.log(Index.checkProfileExist(req.body.uname, req.body.psw));
 
-    console.log(await Index.checkProfileExist(req.body.uname, req.body.psw));
+    var db_res = await Index.checkProfileExist(req.body.uname, req.body.psw);
+    if(db_res.rowCount){
+        
+        req.session.login = 1;
+        req.session.id = db_res.rows[0]._id;
+        req.session.display_name = db_res.rows[0].display_name;
+        req.session.phone_number = db_res.rows[0].phone_number;
+        req.session.email = db_res.rows[0].email;
+        req.session.type = db_res.rows[0].type;
+
+        res.redirect('/dashboard');
+    }
+    else {
+        res.render('main/login', {error_flag: 1})
+    }
+    
+    
 
 }
 
