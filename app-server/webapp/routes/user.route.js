0542c02ab81dfe2c0b46b371a6bd2afbaf6613cb
Doan Anh Tien <d.atien228@gmail.com>
2022-01-07 17:38:14 +0700

    Add user profile, fix device page

--- app-server/webapp/routes/user.route.js
+++ app-server/webapp/routes/user.route.js
@@ -3,6 +3,14 @@ var router = express.Router();
 const userController = require('../controllers/user.controller');
 
 /* GET users listing. */
-router.get('/', userController.userProcessing);
+router.get('/', function(req, res, next) {
+  
+    // Show user
+
+    res.render('main/user');
+    //res.render('index', {result: result});
+});
+
+router.post('/edit', userController.userProcessing);
 
 module.exports = router;
