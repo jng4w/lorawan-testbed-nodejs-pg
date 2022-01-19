02a1fb137abf11d8ef87b227c1e4163321b1fe3e
thiengo <long.hothiengo@hcmut.edu.vn>
2022-01-19 15:08:58 +0700

    update

--- app-server/streaming-broker/acl-garbage-collector/acl-garbage-collector.js
+++ app-server/streaming-broker/acl-garbage-collector/acl-garbage-collector.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const emqx_http = require(`${__dirname}/../emqx/http-api/http-api.js`);
 const common_emqx = JSON.parse(fs.readFileSync(`${__dirname  }/../../common/emqx.json`));
 
-const PERIOD = 2000; //ms
+const PERIOD = 30000; //ms
 const LIST_LIMIT = 1000;
 
 async function collect_garbage() {
