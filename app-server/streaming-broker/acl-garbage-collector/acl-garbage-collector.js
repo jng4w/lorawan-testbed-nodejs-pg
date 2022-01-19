de163955039497b28c63232c52aedcbef7d58966
thiengo <long.hothiengo@hcmut.edu.vn>
2022-01-19 15:07:25 +0700

    update acl garbage collector

--- app-server/streaming-broker/acl-garbage-collector/acl-garbage-collector.js
+++ app-server/streaming-broker/acl-garbage-collector/acl-garbage-collector.js
@@ -5,27 +5,31 @@ const fs = require('fs');
 const emqx_http = require(`${__dirname}/../emqx/http-api/http-api.js`);
 const common_emqx = JSON.parse(fs.readFileSync(`${__dirname  }/../../common/emqx.json`));
 
-const PERIOD = 30000; //ms
+const PERIOD = 2000; //ms
 const LIST_LIMIT = 1000;
 
 async function collect_garbage() {
-    //get client list currently have session and ACL list
-    let client_list = await emqx_http.get_client_list_with_username(common_emqx['ENDUSER_USERNAME'], LIST_LIMIT);
-    let acl_list = await emqx_http.get_acl_list_all_clientid(LIST_LIMIT);
+    try {
+        //get client list currently have session and ACL list
+        let client_list = await emqx_http.get_client_list_with_username(common_emqx['ENDUSER_USERNAME'], LIST_LIMIT);
+        let acl_list = await emqx_http.get_acl_list_all_clientid(LIST_LIMIT);
 
-    //extract client list into clientid array
-    let clientid_list = client_list.data.data.map((client) => {
-        return client.clientid;
-    }); //[<clientid1>, <clientid2>]
+        //extract client list into clientid array
+        let clientid_list = client_list.data.data.map((client) => {
+            return client.clientid;
+        }); //[<clientid1>, <clientid2>]
 
-    //fiter garbage acl by client that do not currently have session and are end-user
-    let garbage_acl_list = acl_list.data.data.filter((acl) => {
-        return !clientid_list.includes(acl.clientid) && (acl.clientid.slice(0, 3) == 'uid');
-    });
+        //fiter garbage acl by client that do not currently have session and are end-user
+        let garbage_acl_list = acl_list.data.data.filter((acl) => {
+            return !clientid_list.includes(acl.clientid) && (acl.clientid.slice(0, 3) == 'uid');
+        });
 
-    garbage_acl_list.forEach(async (acl) => {
-        await emqx_http.delete_acl_clientid_on_topic(acl.clientid, acl.topic);
-    });
+        garbage_acl_list.forEach(async (acl) => {
+            await emqx_http.delete_acl_clientid_on_topic(acl.clientid, acl.topic);
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
   
 setInterval(collect_garbage, PERIOD);
