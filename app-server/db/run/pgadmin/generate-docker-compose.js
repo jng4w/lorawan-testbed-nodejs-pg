2f5b327f665f574f2e2a8e62680eda3e54c832c3
goriummaximum <long.hothiengo@hcmut.edu.vn>
2022-02-15 01:23:23 +0700

    divide docker-compose of pg and pgadmin to 2 different folders

--- app-server/db/run/pgadmin/generate-docker-compose.js
+++ app-server/db/run/pgadmin/generate-docker-compose.js
@@ -1,22 +1,11 @@
 const fs = require('fs');
 
-const common_pg = JSON.parse(fs.readFileSync(`${__dirname}../../common/pg.json`));
-const common_pgadmin = JSON.parse(fs.readFileSync(`${__dirname}../../common/pgadmin.json`));
+const common_pgadmin = JSON.parse(fs.readFileSync(`${__dirname}/../../../common/pgadmin.json`));
 
 const content = 
 `version: \'3.8\'
 
 services:
- timescaledb:
-  container_name: timescaledb_container
-  image: timescale/timescaledb:latest-pg12
-  restart: always
-  environment:
-   POSTGRES_USER: ${common_pg['POSTGRES_USER']}
-   POSTGRES_PASSWORD: ${common_pg['POSTGRES_PASSWORD']}
-  ports:
-   - \"${common_pg['SERVER_PORT']}:${common_pg['SERVER_PORT']}\"
-
  pgadmin:
   container_name: pgadmin4
   image: dpage/pgadmin4
@@ -28,7 +17,6 @@ services:
    PGADMIN_LISTEN_PORT: ${common_pgadmin['SERVER_PORT']}
   ports:
    - \"${common_pgadmin['SERVER_PORT']}:${common_pgadmin['SERVER_PORT']}\"
-
 `;
 
 
