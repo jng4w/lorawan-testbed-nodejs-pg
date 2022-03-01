7a47633d93bda29788c157c1366d3a90cc7cce64
goriummaximum <long.hothiengo@hcmut.edu.vn>
2022-03-01 19:30:54 +0700

    update docker-compose for emqx

--- app-server/streaming-broker/emqx/run/generate-docker-compose.js
+++ app-server/streaming-broker/emqx/run/generate-docker-compose.js
@@ -3,33 +3,45 @@ const fs = require('fs');
 const common_emqx = JSON.parse(fs.readFileSync(`${__dirname}/../../../common/emqx.json`));
 
 const content = 
-`version: \'3\'
+`version: '3'
 
 services:
   emqx1:
     image: emqx/emqx:4.4.0
     environment:
-    - \"EMQX_NAME=emqx\"
-    - \"EMQX_HOST=127.0.0.1\"
-    - \"EMQX_CLUSTER__DISCOVERY=static\"
-    - \"EMQX_CLUSTER__STATIC__SEEDS=emqx@${common_emqx['SERVER_ADDR']}\"
+    - "EMQX_NAME=emqx"
+    - "EMQX_HOST=127.0.0.1"
+    - "EMQX_CLUSTER__DISCOVERY=static"
+    - "EMQX_CLUSTER__STATIC__SEEDS=emqx@${common_emqx['SERVER_ADDR']}"
     networks:
       emqx-bridge:
         aliases:
         - ${common_emqx['SERVER_ADDR']}
     ports:
-      - \"${common_emqx['MQTT_PORT']}:${common_emqx['MQTT_PORT']}\" 
-      - \"${common_emqx['HTTP_API_PORT']}:${common_emqx['HTTP_API_PORT']}\" 
-      - \"${common_emqx['WEBSOCKET_PORT']}:${common_emqx['WEBSOCKET_PORT']}\" 
-      - \"8883:8883\" 
-      - \"8084:8084\" 
-      - \"8080:8080\" 
-      - \"11883:11883\"
-      - \"18083:18083\"
+      - "${common_emqx['MQTT_PORT']}:${common_emqx['MQTT_PORT']}" 
+      - "${common_emqx['HTTP_API_PORT']}:${common_emqx['HTTP_API_PORT']}" 
+      - "${common_emqx['WEBSOCKET_PORT']}:${common_emqx['WEBSOCKET_PORT']}" 
+      - "8883:8883" 
+      - "8084:8084" 
+      - "8080:8080" 
+      - "11883:11883"
+      - "18083:18083"
+    volumes:
+      - vol-emqx-data:/opt/emqx/data
+      - vol-emqx-etc:/opt/emqx/etc
+      - vol-emqx-log:/opt/emqx/log
 
 networks:
   emqx-bridge:
     driver: bridge
+
+volumes:
+  vol-emqx-data:
+    name: vol-emqx-data
+  vol-emqx-etc:
+    name: vol-emqx-etc
+  vol-emqx-log:
+    name: vol-emqx-log
 `;
 
 
